042b8844eb96ff21fc30ad349a96243439da477c
Johann Deneux <johann.deneux@gmail.com>
2011-12-27 19:33:16 +0100

    Initial state construction.

--- MainGame/TestGameComponent.fs
+++ MainGame/TestGameComponent.fs
@@ -9,7 +9,7 @@ open InstancedModel
 
 open SpaceArena2100.GameState
 open SpaceArena2100.GameStateUpdate
-
+open SpaceArena2100.Units
 
 let newDescription() : Description =
     let pos = MarkedArray [| TypedVector3<m>(0.0f<m>, 0.0f<m>, 0.0f<m>) |] 
@@ -53,9 +53,48 @@ let newDescription() : Description =
       asteroids = asteroids;
     }
 
+let newInitialState() : State =
+    let pos = MarkedArray [| TypedVector3<m>(0.0f<m>, 0.0f<m>, -50.0f<m>) |]
+    let ships : Ships =
+        { headings = MarkedArray [| TypedVector3<1>(0.0f, 0.0f, -1.0f) |]
+          rights = MarkedArray [| TypedVector3<1>(1.0f, 0.0f, 0.0f) |]
+          posClient = pos
+          posVisible = pos
+          posHost = pos
+          posLerpT = MarkedArray [| 0.0f |]
+          speeds = MarkedArray [| TypedVector3<m/s>() |]
+          accels = MarkedArray [| TypedVector3<m/s^2>() |]
+          health = MarkedArray [| 1.0f<Health> |]
+          numFastBullets = [ 0 ]
+          numBigBullets = [ 0 ]
+          numMultiFire = [ 0 ]
+          numHighRate = [ 0 ]
+          timeBeforeFire = [ 0<dms> ]
+          timeBeforeRespawn = [ -1<dms> ]
+          localTargetSpeeds = [ 0.0f<m/s> ]
+          scores = MarkedArray [| 0.0f<Points> |]
+        }
+
+    let bullets : Bullets =
+        { guids = [||]
+          pos = [||]
+          timeLeft = [||]
+          speeds = [||]
+          radii = [||]
+          owners = [||]
+        }
+
+    let supplies : Supplies =
+        { pos = MarkedArray [||]
+          types = MarkedArray [||]
+          radii = MarkedArray [||]
+        }
+
+    { ships = ships ; bullets = bullets ; supplies = supplies ; time = 0<dms> }
 
 let newComponent (game : Game) =
-    let initialState = failwith "TODO"
+    let description = newDescription()
+    let initialState = newInitialState()
 
     let content = new Content.ContentManager(game.Services)
     let gdm = new GraphicsDeviceManager(game)
@@ -76,6 +115,10 @@ let newComponent (game : Game) =
     let dispose() =
         content.Dispose()
         (gdm :> System.IDisposable).Dispose()
+
+    let settings =
+        description.localPlayersIdxs
+        |> List.map (fun _ -> ShipControl.defaultSettings)
 
     let update (gt : GameTime) state = state
     let compute (gt : GameTime) state = state
